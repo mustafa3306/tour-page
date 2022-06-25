87c3bbb1fb55f4ac5c4f3e780fffb4a0f3992694
mustafa3306 <mustafaworkkok@gmail.com>
2022-06-25 13:41:53 +0300

    commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.scss"
+import React from "react";
+
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <a href="#aboutus">About Us</a>
+            <a href="#foryou">For You</a>
+            <a href="#services">Services</a>
+            <a href="#blog">Blog</a>
+            <a href="#Contact">Contact</a>
+        </div>
+    )
+}
+
+export default Navbar;
